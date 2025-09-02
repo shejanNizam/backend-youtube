21e1e33e6d10b63cc8f77d39e5e6df3e556eb91a
nizam <nizam.shejan11@gmail.com>
2025-09-02 09:44:33 +0600

    create asyncHandler utils func using Promise

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,14 +1,20 @@
-// const asyncHandler = () => {};
-
-// export { asyncHandler };
-
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.status || 500).json({
-      success: false,
-      message: error.message,
-    });
-  }
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
 };
+
+export { asyncHandler };
+
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (error) {
+//     res.status(error.status || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
