c15cff063820a8c699d5f4d1879c774d70246b06
nizam <nizam.shejan11@gmail.com>
2025-09-09 10:10:10 +0600

    compare pasword useing bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,4 +52,8 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 export const User = mongoose.model("User", userSchema);
