ee854263048f4417361dea95a90a7e46e74ab46c
nizam <nizam.shejan11@gmail.com>
2025-09-09 11:06:58 +0600

    create generateAccessToken & generateRefreshToken methods in userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,4 +59,30 @@ userSchema.methods.isPasswordCorrect = async function (pass) {
   return await bcrypt.compare(pass, this.password);
 };
 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
