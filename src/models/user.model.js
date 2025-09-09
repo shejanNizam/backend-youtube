0cd9447d23a048a4b0e9e27eea25dbba3ff93c08
nizam <nizam.shejan11@gmail.com>
2025-09-09 10:14:45 +0600

    create a isPasswordCorrect in userSchema methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,12 +50,13 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
+
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
+userSchema.methods.isPasswordCorrect = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
 };
 
 export const User = mongoose.model("User", userSchema);
